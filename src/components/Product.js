dd3b6643a14f7a5a3ff03a35e34505f821280164
danish <dannush786@gmail.com>
2021-07-11 11:34:33 +0530

    checkout

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,39 @@
 import Image from "next/image";
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Product({ id, title, price, description, category, image }) {
+
+  const dispatch = useDispatch();
+
+
   const [rating] = useState(
     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   );
 
   const [hasPrime] = useState(Math.random() < 0.5);
+
+
+  const addItemTobasket = () => {
+      const product = {
+        id,
+        title, 
+        price,
+        rating,
+        description, 
+        category, 
+        image,
+        hasPrime,
+      }
+
+      // Sending the product as an action to thr REDUX store.. the basketslice
+      dispatch(addToBasket(product))
+  }
 
   return (
     <div className="relative flex column m-5 bg-white z-30 p-10 ">
@@ -38,7 +61,7 @@ function Product({ id, title, price, description, category, image }) {
         </div>
       )}
 
-      <button className="mt-auto button">Add to Basket</button>
+      <button onClick={addItemTobasket} className="mt-auto button">Add to Basket</button>
     </div>
   );
 }
